204a33e28b2c8636a237570fef088681164e92a6
AppleFE <bagsazadx4@gmail.com>
2026-07-13 02:25:38 +0900

    chore: Forge 1.20.1 모드 프로젝트 구성

---
+++ src/main/java/com/sunlitvalley/chunkyclaim/SocietyChunkyClaimMod.java
@@ -0,0 +1,8 @@
+package com.sunlitvalley.chunkyclaim;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SocietyChunkyClaimMod.MOD_ID)
+public final class SocietyChunkyClaimMod {
+    public static final String MOD_ID = "society_chunkyclaim";
+}
